65755251b4a0ac4519bffd78b9eab0f381c8b4e9
rpinto <rachel.lea.pinto@gmail.com>
2018-07-10 18:48:53 -0700

    finished get endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -4,32 +4,24 @@ const express = require('express');
 
 const router = express.Router();
 
+const Note = require('../models/notes');
+
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
-  console.log('Get All Notes');
-  Notes
-  .find(filter).sort({ updatedAt: 'desc' })
-  .then(notes => {
-    res.json([
-      { id: 1, title: 'Temp 1' },
-      { id: 2, title: 'Temp 2' },
-      { id: 3, title: 'Temp 3' }
-    ]);
-  })
-  .catch(
-    err => {
-      console.error(err);
-      res.status(500).json({message: 'Internal server error'});
-  });
+  
+  Note
+  .find()
+  .then(notes => res.json(notes))
 });
 
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
-
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+  Note
+  .findById(req.params.id)
+  .then(note => {
+    res.json(note)
+  })
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
