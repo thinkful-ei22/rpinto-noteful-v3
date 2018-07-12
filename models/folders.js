b0935cc38861f49cbe34095de72cdea3917cbc0a
rpinto <rachel.lea.pinto@gmail.com>
2018-07-12 12:17:48 -0700

    create folder schema and model

---
+++ models/folders.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true},
+});
+
+// Add `createdAt` and `updatedAt` fields
+noteSchema.set('timestamps', true);
+
+noteSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+ });
+
+ module.exports = mongoose.model('Folder', folderSchema);
